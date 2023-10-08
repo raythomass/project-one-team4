9b6f4e8d5c73beb09ec4782e59c9da2b1c0796c6
LIONELM89 <139692953+LIONELM89@users.noreply.github.com>
2023-10-07 17:53:39 -0700

    Added code for API

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,6 +2,8 @@
 var submitBtn = document.getElementById("search-team-btn")
 var teams = document.getElementById("Teams")
 var playerListingBox = document.getElementById("player-listing-box")
+var ticketbox = document.getElementById("ticketbox")
+
 
 // TICKET_MASTER_API_KEY = "ej5KUdOFhWAlarKWiXJvCsEA8v2JU98K"
 var footballApi = "897300b6bd665bdbe7fd8b164607c7f4" //"2160c0fdbecedae60a649ec139f1f29c"
@@ -9,6 +11,7 @@ var footballApi = "897300b6bd665bdbe7fd8b164607c7f4" //"2160c0fdbecedae60a649ec1
 submitBtn.addEventListener("click", function () {
     var selectedTeam = teams.options[teams.selectedIndex].text;
     getTeam(selectedTeam);
+    getTickets(selectedTeam);
 });
 
 function getTeam(teamName) {
@@ -110,4 +113,39 @@ function displayPlayerStats(selectedPlayerstats) {
     statsUl.append(pos, group, college, number, weight, height, salary);
     statsContainer.append(statsUl);
     
+
+}
+// function for the ticketmaster API.
+function getTickets(selectedTeam) {
+    fetch("https://app.ticketmaster.com/discovery/v2/events.json?locale=en-us&apikey=ej5KUdOFhWAlarKWiXJvCsEA8v2JU98K&classificationName=Football&subGenreId=KZazBEonSMnZfZ7vFE1&keyword=" + selectedTeam)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        displayTickets(data)
+    });
+}
+
+function displayTickets(data) {
+    var ticketsContainer = document.getElementById("tickets")
+    var ticketsUl = document.getElementById("tickets-ul")
+
+    ticketsUl.innerHTML = "";
+
+    var ticket1 = document.createElement("li");
+    var ticket2 = document.createElement("li");
+    var ticket3 = document.createElement("li");
+    var ticket4 = document.createElement("li");
+    var ticket5 = document.createElement("li");
+
+    ticket1.textContent = data._embedded.events[0].name;
+    ticket2.textContent = data._embedded.events[1].name;
+    ticket3.textContent = data._embedded.events[2].name;
+    ticket4.textContent = data._embedded.events[3].name;
+    
+    
+
+    ticketsUl.append(ticket1, ticket2, ticket3, ticket4, ticket5);
+    ticketsContainer.append(ticketsUl);
+    ticketbox.append(ticketsContainer);
 }
